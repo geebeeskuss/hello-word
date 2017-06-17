adebc74e2072454cc171e7795adafe1161865573
LoLoLuke <murtazalieva.albina.04@mail.ru>
2017-06-17 23:57:42 +0300

    Lol

---
+++ strings.py
@@ -0,0 +1,8 @@
+#print("Охота на пончика-велосипедиста началась")
+name = 'Ибра "Игривый"'
+#name2 = "Ибра \"Игривый\""
+print(name[3])
+print(name[0:4])
+print(name[5:14])
+print(name[5])
+print(name[4:15])
